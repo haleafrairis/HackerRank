c6662a3eac0be36a2ae378553d6fe1371e8bbee6
Hale Afra İRİŞ <84947858+haleafraoztekin@users.noreply.github.com>
2022-01-03 17:46:28 +0300

    Update Day 1: Data Types.py

--- Day 1: Data Types.py
+++ Day 1: Data Types.py
@@ -6,30 +6,30 @@
 #   1. Print the sum of i plus your int variable on a new line.
 #   2. Print the sum of d plus your double variable to a scale of one decimal place on a new line.
 #   3. Concatenate s with the string you read as input and print the result on a new line.
-#
+
 # Input Format
-#
+
 # The first line contains an integer that you must sum with i.
 # The second line contains a double that you must sum with d.
 # The third line contains a string that you must concatenate with s.
-#
+
 # Output Format
-#
+
 # Print the sum of both integers on the first line, the sum of both doubles (scaled to 1 decimal place) on the second line, 
 # and then the two concatenated strings on the third line.
-#
+
 # Sample Input:
-# 
+ 
 # 12
 # 4.0
 # is the best place to learn and practice coding!
-#
+
 # Sample Output:
-#
+
 # 16
 # 8.0
 # HackerRank is the best place to learn and practice coding!
-#
+
 # Code
 i = 4
 d = 4.0
