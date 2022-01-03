fe403eba0e2d9cc644f403a748c48b0787c2091c
Hale Afra İRİŞ <84947858+haleafraoztekin@users.noreply.github.com>
2022-01-03 17:47:12 +0300

    Update Day 1: Data Types.py

--- Day 1: Data Types.py
+++ Day 1: Data Types.py
@@ -8,24 +8,20 @@
 #   3. Concatenate s with the string you read as input and print the result on a new line.
 
 # Input Format
-
 # The first line contains an integer that you must sum with i.
 # The second line contains a double that you must sum with d.
 # The third line contains a string that you must concatenate with s.
 
 # Output Format
-
 # Print the sum of both integers on the first line, the sum of both doubles (scaled to 1 decimal place) on the second line, 
 # and then the two concatenated strings on the third line.
 
 # Sample Input:
- 
 # 12
 # 4.0
 # is the best place to learn and practice coding!
 
 # Sample Output:
-
 # 16
 # 8.0
 # HackerRank is the best place to learn and practice coding!
@@ -40,15 +36,16 @@ s = 'HackerRank '
 # You do not need to declare variables before using them, or declare their type. Every variable in Python is an object.
 
 # Read and save an integer, double, and String to your variables.
-
 i_2 = int(input())
 d_2 = float(input())
 s_2 = str(input())
 
 # Print the sum of both integer variables on a new line.
 print(i+i_2)
+
 # Print the sum of the double variables on a new line.
 print(f'{d+d_2:.1f}')
+
 # Concatenate and print the String variables on a new line
 # The 's' variable above should be printed first.
 print(s+s_2)
